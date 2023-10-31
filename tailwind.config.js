d65355cb62973dc2c5e03ef58451a3a3282775ca
CodingFlutter <146336747+WebDevVenus@users.noreply.github.com>
2023-10-31 15:20:57 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefi Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.2em',
+      },
+      colors: {
+        mobNavItemColor: 'rgba(177, 148, 139)',
+        mobNavBarColor: 'rgba(71, 108, 124)',
+        navBarItemColor: 'rgba(2, 20, 50)',
+        heroTextColor: 'rgb(80, 100, 100)',
+      },
+    },
+  },
+  plugins: [],
+}
+
